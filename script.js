3b66c87d045242cad7dda710102f506eae5a3fc0
Hannah Nguyen <hannahng004@gmail.com>
2023-12-06 03:07:26 -0500

    fully set up globe and test info

--- script.js
+++ script.js
@@ -1,54 +1,73 @@
-import * as THREE from "https://cdn.skypack.dev/three@0.136.0";
-import { OrbitControls } from "https://cdn.skypack.dev/three@0.136.0/examples/jsm/controls/OrbitControls";
-import { CSS2DRenderer, CSS2DObject } from 'https://cdn.skypack.dev/three@0.136.0/examples/jsm/renderers/CSS2DRenderer.js';
+// Making sure DOM is loaded first
+document.addEventListener("DOMContentLoaded", function () {
+  let globe;
+  let dataLoaded = false;
+  let places;
 
-globe(document.getElementById('globe-container')).addLayer(labelsLayer).update();
+  // Function to get coordinates of each place
+  function coordinates(placename) {
+    const feature = places.features.find(feature => feature.properties.name === placename);
 
-// Set up scene
-var scene = new THREE.Scene();
+    if (feature) {
+      return {
+        lat: feature.geometry.coordinates[1],
+        lng: feature.geometry.coordinates[0],
+      };
+    } else {
+      console.error(`${placename} not found`);
+      return null;
+    }
+  }
 
-// Set up camera
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 5;
+  // Function to handle landmark click, move globe POV to where landmark is
+  function handleLandmarkClick(landmarkName) {
+    return function (label) {
+      if (dataLoaded && label.properties.name === landmarkName) {
+        globe.pointOfView({
+          lat: coordinates(landmarkName).lat,
+          lng: coordinates(landmarkName).lng,
+          altitude: 2.5
+        }, [1000]);
+      }
+    };
+  }
+  
+  // List of details (lines 36-38, 43-54) from Globe.GL example: https://github.com/vasturiano/globe.gl/blob/master/example/world-cities/index.html
+  // JSON from https://gist.github.com/magamig/a8c0833418fbafd2faa6cfcf9078322c
+  fetch('new7wonders.json')
+    .then(res => res.json())
+    .then(data => {
+      places = data;
+      dataLoaded = true;
+    
+      // Fetching globe and labels appearance details
+      globe = Globe()
+        .globeImageUrl('cloudyearth.jpeg')
+        .backgroundImageUrl('night.jpg')
+        .labelsData(places.features)
+        .labelLat(d => d.geometry.coordinates[1])
+        .labelLng(d => d.geometry.coordinates[0])
+        .labelText(d => d.properties.name)
+        .labelSize(() => 5)
+        .labelDotRadius(() => 5)
+        .labelColor(() => 'rgba(100, 240, 21, 0.75)')
+        .labelResolution(2)
+        .pointOfView({ lat: 0, lng: 0, altitude: 3 })
 
-// Set up renderer
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+        // Globe window
+        (document.getElementById('globeViz'))
+        .height(window.innerHeight)
+        .width(window.innerWidth * 0.75)
 
-// Create globe geometry
-var geometry = new THREE.SphereGeometry(2, 32, 32);
-
-// Load globe texture
-var textureLoader = new THREE.TextureLoader();
-var texture = textureLoader.load('earthtexturemap.jpeg');
-
-// Create material with texture
-var material = new THREE.MeshBasicMaterial({ map: texture });
-
-// Create globe mesh
-var globe = new THREE.Mesh(geometry, material);
-scene.add(globe);
-
-// Set up OrbitControls
-var controls = new OrbitControls(camera, renderer.domElement);
-controls.enableDamping = true; // an animation loop is required when either damping or auto-rotation are enabled
-controls.dampingFactor = 0.25;
-controls.screenSpacePanning = false;
-controls.maxPolarAngle = Math.PI / 2;
-
-// Set up animation
-function animate() {
-  requestAnimationFrame(animate);
-  renderer.render(scene, camera);
-}
-
-// Handle window resize
-window.addEventListener('resize', function () {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth, window.innerHeight);
+        // Globe landmarks for clicking
+        .onLabelClick((label) => {
+          const landmarkName = label.properties.name;
+          if (['Petra', 'Colosseum', 'Taj Mahal', 'Christ the Redeemer', 'Machu Pichu', 'Great Wall of China', 'Chichen Itza'].includes(landmarkName)) {
+              handleLandmarkClick(landmarkName)(label);
+          }
+      })      
+    })
+    // Error catch
+    .catch(error => console.error('Error fetching data:', error))
 });
 
-// Start animation
-animate(); 
