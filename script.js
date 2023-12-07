c64a09cfe3b274a7ecc1a33e9263259412f00800
Hannah Nguyen <hannahng004@gmail.com>
2023-12-07 15:02:02 -0500

    BASICALLY DONE

--- script.js
+++ script.js
@@ -27,11 +27,11 @@ document.addEventListener("DOMContentLoaded", function () {
           lat: coordinates(landmarkName).lat,
           lng: coordinates(landmarkName).lng,
           altitude: 2
-        }, [1000]);
+        }, [1000]);  
       }
     };
   }
-  
+
   // List of details (lines 36-38, 43-54) from Globe.GL example: https://github.com/vasturiano/globe.gl/blob/master/example/world-cities/index.html
   // JSON from https://gist.github.com/magamig/a8c0833418fbafd2faa6cfcf9078322c
   fetch('new7wonders.json')
@@ -42,8 +42,8 @@ document.addEventListener("DOMContentLoaded", function () {
     
       // Fetching globe and labels appearance details
       globe = Globe()
-        .globeImageUrl('img/cloudyearth.jpeg')
-        .backgroundImageUrl('img/night.jpg')
+        .globeImageUrl('templates/img/cloudyearth.jpeg')
+        .backgroundImageUrl('templates/img/night.jpg')
         .labelsData(places.features)
         .labelLat(d => d.geometry.coordinates[1])
         .labelLng(d => d.geometry.coordinates[0])
@@ -59,15 +59,30 @@ document.addEventListener("DOMContentLoaded", function () {
         .height(window.innerHeight * 0.90)
         .width(window.innerWidth * 0.70)
         
+        document.querySelectorAll('iframe').forEach(iframe => iframe.style.display = 'none');
+
         // Globe landmarks for clicking
         globe.onLabelClick((label) => {
           const landmarkName = label.properties.name;
           
           // Set background image dynamically based on landmarkName
-          globe.backgroundImageUrl(`img/${landmarkName.toLowerCase()}.jpg`);
+          globe.backgroundImageUrl(`templates/img/${landmarkName}.jpg`);
           
           // Moving POV to landmark
           handleLandmarkClick(landmarkName)(label);
+          
+          const iframe = document.getElementById(`${landmarkName}_iframe`);
+          if (iframe) {
+            iframe.style.display = 'block';
+          }
+        
+          // Scroll to corresponding HTML id
+          const targetElement = document.getElementById(landmarkName);
+          if (targetElement) {
+            targetElement.scrollIntoView({ behavior: 'smooth' });
+          }
+          
+        
         });
       })
 
